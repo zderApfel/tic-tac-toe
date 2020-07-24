4245ee0229bbe8d1fbe18b9c45a507c0ca9cda27
zderApfel <scoutt172@yahoo.com>
2020-07-24 17:33:14 -0400

    Comment push for troubleshooting

--- menu.js
+++ menu.js
@@ -1,21 +1,25 @@
 const Menu = () => {
-    let playerOne = document.getElementById("player-1");
-    let playerTwo = document.getElementById("player-2");
+    let playerOneInput = document.getElementById("player-1");
+    let playerTwoInput = document.getElementById("player-2");
     let playButton = document.getElementById("play-button");
+    let playerOne = "";
+    let playerTwo = "";
 
     const init = () => {
         playButton.addEventListener("click", function() {
-            if (playerOne.value == "" || playerTwo.value == ""){
+            if (playerOneInput.value == "" || playerTwoInput.value == ""){
                 alert("You forgot to enter a name!");
             }
             else{
                 location.href="game.html";
+                playerOne = playerOneInput.value; // I am trying to get this:
+                playerTwo = playerTwoInput.value; // and this to game.js, which is operating on the game.html file. But every method I've used does not work
             }
-        })
+        });
     }
-    return { init };
+    return { init, playerOne, playerTwo };
 }
 
 const initialize = Menu();
-
 initialize.init();
+
