09752c56b7c19dc4da086609e3f17aa1b1a03676
zderApfel <scoutt172@yahoo.com>
2020-07-27 16:57:10 -0400

    Scoreboard populates, Player object defined

--- game.js
+++ game.js
@@ -1,15 +1,30 @@
 function Player(playerName, score, symbol) {
     this.playerName = playerName;
     this.score = score;
-    this.nameBoard = document.getElementById(`${symbol}-player`).innerHTML = this.playerName;
-    this.scoreBoard = document.getElementById(`${symbol}-score`)
-    this.scoreBoard.innerHTML = this.score;
+    this.nameBoard = document.getElementById(`${symbol}-player`);
+    this.scoreBoard = document.getElementById(`${symbol}-score`);
 }
 
-const ActivateBoard = () => {
+const TicTacToe = () => {
     const PLAYER_1 = new Player(localStorage.getItem("player1"), 0, "o");
     const PLAYER_2 = new Player(localStorage.getItem("player2"), 0, "x");
+    
+    const initialize = () => {
+        PLAYER_1.nameBoard.innerHTML = PLAYER_1.playerName;
+        PLAYER_2.nameBoard.innerHTML = PLAYER_2.playerName;
+        PLAYER_1.scoreBoard.innerHTML = PLAYER_1.score;
+        PLAYER_2.scoreBoard.innerHTML = PLAYER_2.score;
+    }
+    
+    const changeScore = (player, amount) => {
+        player.score = player.score + amount;
+        player.scoreBoard.innerHTML = player.score;
+    }
+
+    return { initialize }
 }
 
-const begin = ActivateBoard();
+const begin = TicTacToe();
+
+begin.initialize();
 
