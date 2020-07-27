1f39c0207c47b8711304911b6652304ea247f779
zderApfel <scoutt172@yahoo.com>
2020-07-27 12:16:36 -0400

    Board now initializes with player names and blank score

--- game.js
+++ game.js
@@ -1,4 +1,18 @@
-const PLAYER_ONE = localStorage.getItem("player1");
-const PLAYER_TWO = localStorage.getItem("player2");
+function Player(playerName, score, symbol) {
+    this.playerName = playerName;
+    this.score = score;
+    this.nameBoard = document.getElementById(`${symbol}-player`).innerHTML = this.playerName;
+    this.scoreBoard = document.getElementById(`${symbol}-score`)
+    this.scoreBoard.innerHTML = this.score;
+    const changeScore = (amount) => {
+        this.score = this.score + 1;
+        this.scoreBoard.innerHTML = this.score;
+    }
+}
 
-console.log(`${PLAYER_ONE} ${PLAYER_TWO}`);
+const ActivateBoard = () => {
+    const PLAYER_1 = new Player(localStorage.getItem("player1"), 0, "o");
+    const PLAYER_2 = new Player(localStorage.getItem("player2"), 0, "x");
+}
+
+const begin = ActivateBoard();
